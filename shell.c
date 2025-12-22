356ba3e56241c956160a88c990efee5ae60f1313
0-Mousa-0 <mossa.alqarni01@gmail.com>
2025-12-22 17:25:23 +0000

    Fix simple_shell: handle spaces, EOF, execve, and C90 compliance

--- shell.c
+++ shell.c
@@ -6,88 +6,75 @@
 
 extern char **environ;
 
-/**
- * trim_spaces - remove leading and trailing spaces
- * @str: input string
- * Return: pointer to trimmed string (modifies in place)
- */
 char *trim_spaces(char *str)
 {
-    char *end;
-
-    while (*str == ' ' || *str == '\t')
-        str++;
-
-    if (*str == 0)
-        return str;
-
-    end = str + strlen(str) - 1;
-    while (end > str && (*end == ' ' || *end == '\t'))
-        end--;
-
-    *(end + 1) = '\0';
-
-    return str;
+char *end;
+while (*str == ' ' || *str == '\t')
+str++;
+if (*str == 0)
+return str;
+end = str + strlen(str) - 1;
+while (end > str && (*end == ' ' || *end == '\t'))
+end--;
+*(end + 1) = '\0';
+return str;
 }
 
-/**
- * main - simple UNIX shell
- * Return: 0
- */
 int main(void)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
-    pid_t pid;
-    int status;
-    char *argv[2];
+char *line = NULL;
+size_t len = 0;
+ssize_t nread;
+pid_t pid;
+int status;
+char *argv[2];
+char *command;
+char *first_word;
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO))
-            write(STDOUT_FILENO, "#cisfun$ ", 9);
+while (1)
+{
+if (isatty(STDIN_FILENO))
+write(STDOUT_FILENO, "#cisfun$ ", 9);
 
-        nread = getline(&line, &len, stdin);
+nread = getline(&line, &len, stdin);
+if (nread == -1)
+{
+free(line);
+exit(0);
+}
 
-        if (nread == -1)
-        {
-            free(line);
-            exit(0);
-        }
+if (line[nread - 1] == '\n')
+line[nread - 1] = '\0';
 
-        if (line[nread - 1] == '\n')
-            line[nread - 1] = '\0';
+command = trim_spaces(line);
+if (*command == '\0')
+continue;
 
-        line = trim_spaces(line);
+first_word = strtok(command, " \t");
+if (!first_word)
+continue;
 
-        if (*line == '\0')
-            continue;
+pid = fork();
+if (pid == -1)
+{
+perror("fork");
+continue;
+}
 
-        pid = fork();
-        if (pid == -1)
-        {
-            perror("fork");
-            continue;
-        }
+if (pid == 0)
+{
+argv[0] = first_word;
+argv[1] = NULL;
+if (execve(argv[0], argv, environ) == -1)
+{
+fprintf(stderr, "%s: No such file or directory\n", argv[0]);
+exit(1);
+}
+}
+else
+wait(&status);
+}
 
-        if (pid == 0)
-        {
-            argv[0] = line;
-            argv[1] = NULL;
-
-            if (execve(argv[0], argv, environ) == -1)
-            {
-                perror("./shell");
-                exit(1);
-            }
-        }
-        else
-        {
-            wait(&status);
-        }
-    }
-
-    free(line);
-    return 0;
+free(line);
+return 0;
 }
