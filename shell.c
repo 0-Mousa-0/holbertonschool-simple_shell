5184d5b57a7b87f4f6b3149b15b1bad450b6557d
0-Mousa-0 <mossa.alqarni01@gmail.com>
2025-12-22 17:09:03 +0000

    Fix simple_shell: handle spaces, EOF, and execve with environ correctly

--- shell.c
+++ shell.c
@@ -1,60 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/wait.h>
 
 extern char **environ;
 
+/**
+ * trim_spaces - remove leading and trailing spaces
+ * @str: input string
+ * Return: pointer to trimmed string (modifies in place)
+ */
+char *trim_spaces(char *str)
+{
+    char *end;
+
+    while (*str == ' ' || *str == '\t')
+        str++;
+
+    if (*str == 0)
+        return str;
+
+    end = str + strlen(str) - 1;
+    while (end > str && (*end == ' ' || *end == '\t'))
+        end--;
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+/**
+ * main - simple UNIX shell
+ * Return: 0
+ */
 int main(void)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t nread;
-	pid_t pid;
-	int status;
-	char *argv[2];
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    pid_t pid;
+    int status;
+    char *argv[2];
 
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "#cisfun$ ", 9);
+    while (1)
+    {
+        if (isatty(STDIN_FILENO))
+            write(STDOUT_FILENO, "#cisfun$ ", 9);
 
-		nread = getline(&line, &len, stdin);
+        nread = getline(&line, &len, stdin);
 
-		if (nread == -1)
-		{
-			free(line);
-			exit(0);
-		}
+        if (nread == -1)
+        {
+            free(line);
+            exit(0);
+        }
 
-		if (line[nread - 1] == '\n')
-			line[nread - 1] = '\0';
+        if (line[nread - 1] == '\n')
+            line[nread - 1] = '\0';
 
-		if (*line == '\0')
-			continue;
+        line = trim_spaces(line);
 
-		pid = fork();
+        if (*line == '\0')
+            continue;
 
-		if (pid == -1)
-		{
-			perror("fork");
-			continue;
-		}
+        pid = fork();
+        if (pid == -1)
+        {
+            perror("fork");
+            continue;
+        }
 
-		if (pid == 0)
-		{
-			argv[0] = line;
-			argv[1] = NULL;
+        if (pid == 0)
+        {
+            argv[0] = line;
+            argv[1] = NULL;
 
-			if (execve(line, argv, environ) == -1)
-			{
-				perror("./shell");
-				exit(1);
-			}
-		}
-		else
-		{
-			wait(&status);
-		}
-	}
+            if (execve(argv[0], argv, environ) == -1)
+            {
+                perror("./shell");
+                exit(1);
+            }
+        }
+        else
+        {
+            wait(&status);
+        }
+    }
+
+    free(line);
+    return 0;
 }
