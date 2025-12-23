2b152d5256eee89e8099ca848997867fdee399b7
0-Mousa-0 <mosa.alqarni01@gmail.com>
2025-12-23 16:42:13 +0000

    implement shell helper functions

--- shell.c
+++ shell.c
@@ -1,13 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
+#include "shell.h"
 
-extern char **environ;
-
-/* Trim spaces at start and end */
+/* Remove leading and trailing spaces/tabs */
 char *trim_spaces(char *str)
 {
     char *end;
@@ -68,78 +61,39 @@ char *get_full_path(char *cmd)
     return NULL;
 }
 
-int main(void)
+/* Fork and execute a command */
+void execute_command(char **argv)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
-    char *argv[64];
-    char *command;
-    int i;
     pid_t pid;
     int status;
-    char *full_path;
-    while (1)
+
+    char *full_path = get_full_path(argv[0]);
+    if (!full_path)
     {
-        if (isatty(STDIN_FILENO))
-            write(STDOUT_FILENO, ":) ", 3);
-
-        nread = getline(&line, &len, stdin);
-        if (nread == -1)
-        {
-            free(line);
-            exit(0);
-        }
-
-        if (line[nread - 1] == '\n')
-            line[nread - 1] = '\0';
-
-        command = trim_spaces(line);
-        if (*command == '\0')
-            continue;
-
-        /* Split command into argv */
-        i = 0;
-        argv[i] = strtok(command, " \t");
-        while (argv[i] && i < 63)
-        {
-            i++;
-            argv[i] = strtok(NULL, " \t");
-        }
-        argv[i] = NULL;
-
-        /* Get full path from PATH */
-        full_path = get_full_path(argv[0]);
-        if (!full_path)
-        {
-            fprintf(stderr, "%s: command not found\n", argv[0]);
-            continue; /* skip fork */
-        }
-
-        pid = fork();
-        if (pid == -1)
-        {
-            perror("fork");
-            free(full_path);
-            continue;
-        }
-
-        if (pid == 0)
-        {
-            if (execve(full_path, argv, environ) == -1)
-            {
-                perror("execve");
-                exit(1);
-            }
-        }
-        else
-        {
-            wait(&status);
-        }
-
-        free(full_path);
+        fprintf(stderr, "%s: command not found\n", argv[0]);
+        return; /* skip fork */
     }
 
-    free(line);
-    return 0;
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("fork");
+        free(full_path);
+        return;
+    }
+
+    if (pid == 0)
+    {
+        if (execve(full_path, argv, environ) == -1)
+        {
+            perror("execve");
+            exit(1);
+        }
+    }
+    else
+    {
+        wait(&status);
+    }
+
+    free(full_path);
 }
