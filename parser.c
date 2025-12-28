a1300d63c805e36fb7dbfc58e14720563cdf179e
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-12-28 16:54:59 +0000

    Input parsing functions to split commands into arguments

--- parser.c
+++ parser.c
@@ -7,46 +7,40 @@
  */
 char **parse_input(char *input)
 {
-    int bufsize = 64;
-    int position = 0;
-    char **args = malloc(bufsize * sizeof(char *));
-    char *token;
-    
-    if (!args)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        return (NULL);
-    }
-    
-    token = strtok(input, " \t\r\n");
-    while (token != NULL)
-    {
-        if (token[0] != '\0')
-        {
-            args[position] = _strdup(token);
-            if (!args[position])
-            {
-                free_args(args);
-                return (NULL);
-            }
-            position++;
-            
-            if (position >= bufsize)
-            {
-                bufsize += 64;
-                args = realloc(args, bufsize * sizeof(char *));
-                if (!args)
-                {
-                    fprintf(stderr, "Error: malloc failed\n");
-                    return (NULL);
-                }
-            }
-        }
-        token = strtok(NULL, " \t\r\n");
-    }
-    
-    args[position] = NULL;
-    return (args);
+int bufsize = 64;
+int position = 0;
+char **args = malloc(bufsize * sizeof(char *));
+char *token;
+
+if (!args)
+return (NULL);
+
+token = strtok(input, " \t\r\n");
+while (token != NULL)
+{
+if (token[0] != '\0')
+{
+args[position] = _strdup(token);
+if (!args[position])
+{
+free_args(args);
+return (NULL);
+}
+position++;
+
+if (position >= bufsize)
+{
+bufsize += 64;
+args = realloc(args, bufsize * sizeof(char *));
+if (!args)
+return (NULL);
+}
+}
+token = strtok(NULL, " \t\r\n");
+}
+
+args[position] = NULL;
+return (args);
 }
 
 /**
@@ -56,16 +50,16 @@ char **parse_input(char *input)
  */
 int is_builtin(char *command)
 {
-    char *builtins[] = {"exit", "env", "cd", NULL};
-    int i;
-    
-    if (!command)
-        return (0);
-    
-    for (i = 0; builtins[i]; i++)
-    {
-        if (_strcmp(command, builtins[i]) == 0)
-            return (1);
-    }
-    return (0);
+char *builtins[] = {"exit", "env", "cd", NULL};
+int i;
+
+if (!command)
+return (0);
+
+for (i = 0; builtins[i]; i++)
+{
+if (_strcmp(command, builtins[i]) == 0)
+return (1);
+}
+return (0);
 }
