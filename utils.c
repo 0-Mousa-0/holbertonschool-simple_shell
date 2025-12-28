d74dd702b072d8b263d238ff8a22150156627935
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-12-28 16:56:31 +0000

    Utility functions for I/O, error messages, and built-in commands

--- utils.c
+++ utils.c
@@ -5,8 +5,8 @@
  */
 void display_prompt(void)
 {
-    printf("$ ");
-    fflush(stdout);
+printf("$ ");
+fflush(stdout);
 }
 
 /**
@@ -15,21 +15,21 @@ void display_prompt(void)
  */
 char *read_input(void)
 {
-    char *line = NULL;
-    size_t bufsize = 0;
-    ssize_t nread;
-    
-    nread = getline(&line, &bufsize, stdin);
-    if (nread == -1)
-    {
-        free(line);
-        return (NULL);
-    }
-    
-    if (nread > 0 && line[nread - 1] == '\n')
-        line[nread - 1] = '\0';
-    
-    return (line);
+char *line = NULL;
+size_t bufsize = 0;
+ssize_t nread;
+
+nread = getline(&line, &bufsize, stdin);
+if (nread == -1)
+{
+free(line);
+return (NULL);
+}
+
+if (nread > 0 && line[nread - 1] == '\n')
+line[nread - 1] = '\0';
+
+return (line);
 }
 
 /**
@@ -38,25 +38,25 @@ char *read_input(void)
  */
 void free_args(char **args)
 {
-    int i;
-    
-    if (!args)
-        return;
-    
-    for (i = 0; args[i] != NULL; i++)
-        free(args[i]);
-    
-    free(args);
+int i;
+
+if (!args)
+return;
+
+for (i = 0; args[i] != NULL; i++)
+free(args[i]);
+
+free(args);
 }
 
 /**
- * print_error - Print error message in required format
+ * print_error - Print error message
  * @program: Program name
  * @command: Command that failed
  */
 void print_error(char *program, char *command)
 {
-    fprintf(stderr, "%s: 1: %s: not found\n", program, command);
+fprintf(stderr, "%s: 1: %s: not found\n", program, command);
 }
 
 /**
@@ -66,25 +66,25 @@ void print_error(char *program, char *command)
  */
 int change_dir(char **args)
 {
-    char *dir = args[1];
-    char cwd[PATH_MAX];
-    char *home;
-    
-    if (dir == NULL)
-    {
-        home = _getenv("HOME");
-        if (home)
-            dir = home;
-        else
-            dir = "/";
-    }
-    
-    if (chdir(dir) != 0)
-    {
-        fprintf(stderr, "cd: can't cd to %s\n", dir);
-        return (1);
-    }
-    
-    getcwd(cwd, sizeof(cwd)); /* Just to avoid warning */
-    return (1);
+char *dir = args[1];
+char cwd[PATH_MAX];
+char *home;
+
+if (dir == NULL)
+{
+home = _getenv("HOME");
+if (home)
+dir = home;
+else
+dir = "/";
+}
+
+if (chdir(dir) != 0)
+{
+fprintf(stderr, "cd: can't cd to %s\n", dir);
+return (1);
+}
+
+getcwd(cwd, sizeof(cwd));
+return (1);
 }
