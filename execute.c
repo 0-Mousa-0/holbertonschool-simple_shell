a2172347edeb32dcc39908c1ad79af9a1ecb9b68
0-Mousa-0 <mosa.alqarni01@gmail.com>
2025-12-28 13:41:55 +0000

    Add missing handle_builtin and other built-in functions

--- execute.c
+++ execute.c
@@ -1,63 +1,9 @@
 #include "shell.h"
 
 /**
- * execute_command - Execute a command
- * @args: Array of arguments
- * Return: 1 to continue, 0 to exit
- */
-int execute_command(char **args)
-{
-    pid_t pid;
-    int status;
-    char *full_path;
-    
-    if (args[0] == NULL)
-        return (1);
-    
-    /* Handle built-in commands */
-    if (is_builtin(args[0]))
-        return (handle_builtin(args));
-    
-    /* Find command in PATH before forking */
-    full_path = find_in_path(args[0]);
-    if (full_path == NULL)
-    {
-        print_error("hsh", args[0]);
-        return (1);
-    }
-    
-    /* Command exists, now fork */
-    pid = fork();
-    if (pid == 0)
-    {
-        /* Child process */
-        if (execve(full_path, args, environ) == -1)
-        {
-            perror("hsh");
-            free(full_path);
-            exit(EXIT_FAILURE);
-        }
-    }
-    else if (pid < 0)
-    {
-        /* Fork failed */
-        perror("hsh");
-        free(full_path);
-    }
-    else
-    {
-        /* Parent process */
-        waitpid(pid, &status, 0);
-        free(full_path);
-    }
-    
-    return (1);
-}
-
-/**
  * handle_builtin - Handle built-in commands
  * @args: Array of arguments
- * Return: 1 to continue, 0 to exit
+ * Return: 1 or 0
  */
 int handle_builtin(char **args)
 {
@@ -67,12 +13,14 @@ int handle_builtin(char **args)
         return (print_env(args));
     else if (_strcmp(args[0], "cd") == 0)
         return (change_dir(args));
+    else if (_strcmp(args[0], "help") == 0)
+        return (print_help(args));
     
     return (1);
 }
 
 /**
- * exit_shell - Exit the shell
+ * exit_shell - Exit shell
  * @args: Arguments
  * Return: 0 to exit
  */
@@ -97,6 +45,54 @@ int print_env(char **args)
     {
         printf("%s\n", *env);
         env++;
+    }
+    
+    return (1);
+}
+
+/**
+ * execute_command - Execute a command
+ * @args: Array of arguments
+ * Return: 1 to continue, 0 to exit
+ */
+int execute_command(char **args)
+{
+    pid_t pid;
+    int status;
+    char *full_path;
+    
+    if (args[0] == NULL)
+        return (1);
+    
+    if (is_builtin(args[0]))
+        return (handle_builtin(args));
+    
+    full_path = find_executable(args[0]);
+    if (full_path == NULL)
+    {
+        print_error("hsh", args[0]);
+        return (1);
+    }
+    
+    pid = fork();
+    if (pid == 0)
+    {
+        if (execve(full_path, args, environ) == -1)
+        {
+            print_error("hsh", args[0]);
+            free(full_path);
+            _exit(EXIT_FAILURE);
+        }
+    }
+    else if (pid < 0)
+    {
+        perror("hsh");
+        free(full_path);
+    }
+    else
+    {
+        waitpid(pid, &status, 0);
+        free(full_path);
     }
     
     return (1);
