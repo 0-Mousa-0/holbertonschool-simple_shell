f83254722443984d838728ffd0715981a5778f70
0-Mousa-0 <mosa.alqarni01@gmail.com>
2025-12-28 13:41:03 +0000

    Complete header with all function prototypes

--- shell.h
+++ shell.h
@@ -11,12 +11,9 @@
 #include <errno.h>
 #include <limits.h>
 
-/* Global environment */
 extern char **environ;
 
 /* Function prototypes */
-
-/* Main shell functions */
 void display_prompt(void);
 char *read_input(void);
 char **parse_input(char *input);
@@ -27,21 +24,28 @@ int handle_builtin(char **args);
 int exit_shell(char **args);
 int print_env(char **args);
 int change_dir(char **args);
+int print_help(char **args);
 
-/* Path handling functions */
+/* Path handling */
+char *find_executable(char *command);
 char *find_in_path(char *command);
-int check_command_exists(char *command);
 
 /* Utility functions */
 void free_args(char **args);
 void print_error(char *program, char *command);
 int is_builtin(char *command);
 
-/* Custom string functions */
-int _strlen(char *s);
-char *_strdup(char *str);
-int _strcmp(char *s1, char *s2);
-char *_strcpy(char *dest, char *src);
-char *_strcat(char *dest, char *src);
+/* String functions */
+int _strlen(const char *s);
+char *_strdup(const char *str);
+int _strcmp(const char *s1, const char *s2);
+char *_strcpy(char *dest, const char *src);
+char *_strcat(char *dest, const char *src);
+int _strncmp(const char *s1, const char *s2, size_t n);
+char *_getenv(const char *name);
+
+/* Helper functions */
+int is_path_command(const char *command);
+char *build_full_path(const char *dir, const char *command);
 
 #endif
