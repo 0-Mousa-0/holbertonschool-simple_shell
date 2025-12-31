a6eefd3a35b5d183c2cd0c3802fadc6dc7a3a574
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-12-31 03:25:09 +0000

    exit built-in that exits shell when called

--- execute.c
+++ execute.c
@@ -1,10 +1,22 @@
 #include "shell.h"
 
+/**
+ * handle_builtin - Handle built-in commands
+ * @args: Array of arguments
+ * Return: 0 for exit, 1 to continue
+ */
+int handle_builtin(char **args)
+{
+if (_strcmp(args[0], "exit") == 0)
+return (exit_shell(args));
+
+return (1);
+}
 
 /**
  * exit_shell - Exit shell
  * @args: Arguments
- * Return: 0 to signal exit
+ * Return: 0 (signals exit)
  */
 int exit_shell(char **args)
 {
@@ -13,48 +25,33 @@ return (0);
 }
 
 /**
- * print_env - Print environment variables
- * @args: Arguments
- * Return: 1
- */
-int print_env(char **args)
-{
-char **env = environ;
-
-(void)args;
-
-while (*env)
-{
-printf("%s\n", *env);
-env++;
-}
-
-return (1);
-}
-
-/**
  * execute_command - Execute a command
  * @args: Array of arguments
  * @prog_name: Program name (argv[0])
- * Return: Exit status of command, or 0 for exit built-in
+ * Return: Exit status of command, 0 for exit
  */
 int execute_command(char **args, char *prog_name)
 {
 pid_t pid;
 int status;
 char *full_path;
+
 if (args[0] == NULL)
 return (0);
+
 if (is_builtin(args[0]))
 return (handle_builtin(args));
+
 full_path = find_executable(args[0]);
 if (full_path == NULL)
 {
 print_error(prog_name, args[0]);
-return (127);  /* Command not found - return 127 */
+return (127);
 }
+
 pid = fork();
 if (pid == 0)
+{
 if (execve(full_path, args, environ) == -1)
 {
 print_error(prog_name, args[0]);
@@ -72,10 +69,13 @@ else
 {
 waitpid(pid, &status, 0);
 free(full_path);
+
 if (WIFEXITED(status))
-return (WEXITSTATUS(status));  /* Return actual exit status */
-else
+{
+return (WEXITSTATUS(status));
+}
 return (1);
 }
+
 return (0);
 }
