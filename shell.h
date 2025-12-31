b4d93b170c5d841f075bddf0162ca38b5a819ed4
0-Mousa-0 <mosa.alqarni01@gmail.com>
2025-12-31 12:40:20 +0300

    Add print_error function prototype

--- shell.h
+++ shell.h
@@ -3,26 +3,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-#include <errno.h>
 
-extern char **environ;
-
-void display_prompt(void);
-char *read_input(void);
-char **parse_input(char *input);
-int execute_command(char **args, char *prog_name);
+/* Function prototypes */
+char *read_line(void);
+char **parse_line(char *line);
+int execute_command(char **args);
+char *find_command(char *command);
 void free_args(char **args);
-int _strcmp(char *s1, char *s2);
-int _strlen(char *s);
-char *_strdup(char *str);
-char *_strcpy(char *dest, char *src);
-char *_strcat(char *dest, char *src);
-int has_slash(char *command);
-char *find_in_path(char *command);
+int is_builtin(char *command);
+int handle_builtin(char **args);
+void print_error(char *command);
+
+/* Built-in commands */
+int shell_exit(char **args);
+int shell_env(char **args);
+
+/* Environment */
+extern char **environ;
 
 #endif
