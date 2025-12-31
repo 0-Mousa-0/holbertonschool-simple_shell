971d644f474982fcc40a7ea27324795ddb30ccad
0-Mousa-0 <mosa.alqarni01@gmail.com>
2025-12-31 13:29:40 +0300

    handle NULL environment and missing PATH variable in _getenv

--- utils2.c
+++ utils2.c
@@ -1,41 +1,28 @@
 #include "shell.h"
 
+/**
+ * _getenv - Gets the value of an environment variable
+ * @name: Name of the variable (e.g., "PATH")
+ * Return: Value of the variable or NULL if not found/empty
+ */
 char *_getenv(const char *name)
 {
 	int i = 0;
-	size_t len = _strlen((char *)name);
+	size_t len;
 
+	if (name == NULL || environ == NULL)
+		return (NULL);
+
+	len = _strlen((char *)name);
 	while (environ[i])
 	{
+		/* Check if variable starts with 'name=' */
 		if (strncmp(environ[i], name, len) == 0 && environ[i][len] == '=')
+		{
 			return (environ[i] + len + 1);
+		}
 		i++;
 	}
 	return (NULL);
 }
-
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-char *_strcat(char *dest, char *src)
-{
-	int i = 0, j = 0;
-	while (dest[i]) i++;
-	while (src[j])
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
+/* ... _strcpy and _strcat follow here ... */
