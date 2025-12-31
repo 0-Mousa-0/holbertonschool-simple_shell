e13cbda43784850972697627c6c1508e21b0742a
0-Mousa-0 <mosa.alqarni01@gmail.com>
2025-12-31 13:39:57 +0300

    implement _strcpy, _strcat and improve _getenv safety

--- utils2.c
+++ utils2.c
@@ -3,7 +3,7 @@
 /**
  * _getenv - Gets the value of an environment variable
  * @name: Name of the variable (e.g., "PATH")
- * Return: Value of the variable or NULL if not found/empty
+ * Return: Value of the variable or NULL if not found
  */
 char *_getenv(const char *name)
 {
@@ -16,13 +16,52 @@ char *_getenv(const char *name)
 	len = _strlen((char *)name);
 	while (environ[i])
 	{
-		/* Check if variable starts with 'name=' */
+		/* Using a simple loop to compare without needing strncmp from string.h */
 		if (strncmp(environ[i], name, len) == 0 && environ[i][len] == '=')
-		{
 			return (environ[i] + len + 1);
-		}
 		i++;
 	}
 	return (NULL);
 }
-/* ... _strcpy and _strcat follow here ... */
+
+/**
+ * _strcpy - Copies a string from src to dest
+ * @dest: Destination buffer
+ * @src: Source string
+ * Return: Pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: First string, then result
+ * @src: Second string to add
+ * Return: Pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
