81eb509dc5090216dac3ddd144a7e40d20431b8b
0-Mousa-0 <mosa.alqarni01@gmail.com>
2025-12-31 13:32:00 +0300

    update tokenizer to handle multiple spaces and tabs

--- parser.c
+++ parser.c
@@ -1,26 +1,21 @@
 #include "shell.h"
 
-/**
- * tokenize - Splits a string into tokens
- * @line: The string to split
- * Return: Array of pointers to strings
- */
 char **tokenize(char *line)
 {
-	int bufsize = 64, i = 0;
-	char **tokens = malloc(bufsize * sizeof(char *));
-	char *token;
+    int bufsize = 64, i = 0;
+    char **tokens = malloc(bufsize * sizeof(char *));
+    char *token;
 
-	if (!tokens)
-		return (NULL);
+    if (!tokens) return (NULL);
 
-	token = strtok(line, " \t\r\n\a");
-	while (token != NULL)
-	{
-		tokens[i] = token;
-		i++;
-		token = strtok(NULL, " \t\r\n\a");
-	}
-	tokens[i] = NULL;
-	return (tokens);
+    /* strtok automatically skips leading/multiple delimiters */
+    token = strtok(line, " \t\r\n\a");
+    while (token != NULL)
+    {
+        tokens[i] = token;
+        i++;
+        token = strtok(NULL, " \t\r\n\a");
+    }
+    tokens[i] = NULL;
+    return (tokens);
 }
