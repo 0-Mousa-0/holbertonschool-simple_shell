65cb3f2f07a307f5ee9e86ded2c12445d654dc1c
0-Mousa-0 <mosa.alqarni01@gmail.com>
2025-12-31 10:49:04 +0300

    built-in that exits shell when called

--- execute.c
+++ execute.c
@@ -1,30 +1,29 @@
 #include "shell.h"
 
-/**
- * execute_command - Execute a command
- * @args: Array of arguments
- * @prog_name: Program name (argv[0])
- * Return: Exit status of command
- */
 int execute_command(char **args, char *prog_name)
 {
 pid_t pid;
 int status;
-struct stat st;
+char *full_path;
 
 if (args[0] == NULL)
 return (0);
 
-if (strchr(args[0], '/') != NULL)
+if (_strcmp(args[0], "exit") == 0)
+return (0);
+
+/* Check if command contains '/' */
+if (has_slash(args[0]))
 {
-if (stat(args[0], &st) == 0 && (st.st_mode & S_IXUSR))
+/* Absolute or relative path - check if exists */
+if (access(args[0], F_OK) == 0)
 {
+/* Command exists - fork and execute */
 pid = fork();
 if (pid == 0)
 {
 if (execve(args[0], args, environ) == -1)
 {
-/* This shouldn't happen if stat succeeded, but just in case */
 fprintf(stderr, "%s: 1: %s: not found\n", prog_name, args[0]);
 _exit(127);
 }
@@ -38,38 +37,52 @@ else
 {
 waitpid(pid, &status, 0);
 if (WIFEXITED(status))
-{
 return (WEXITSTATUS(status));
-}
 return (1);
 }
 }
 else
 {
-/* Command exists but not executable, or doesn't exist */
-/* Let execve handle the error and return proper status */
+/* Command with '/' doesn't exist - NO FORK */
+fprintf(stderr, "%s: 1: %s: not found\n", prog_name, args[0]);
+return (127);
+}
+}
+
+/* Command without '/' - search in PATH */
+full_path = find_in_path(args[0]);
+if (full_path == NULL)
+{
+/* Command not found in PATH - NO FORK */
+fprintf(stderr, "%s: 1: %s: not found\n", prog_name, args[0]);
+return (127);
+}
+
+/* Command found - fork and execute */
 pid = fork();
 if (pid == 0)
 {
-execve(args[0], args, environ);
-/* If execve fails, it won't return */
-exit(127);
+if (execve(full_path, args, environ) == -1)
+{
+free(full_path);
+fprintf(stderr, "%s: 1: %s: not found\n", prog_name, args[0]);
+_exit(127);
+}
 }
 else if (pid < 0)
 {
+free(full_path);
 perror(prog_name);
 return (1);
 }
 else
 {
 waitpid(pid, &status, 0);
+free(full_path);
 if (WIFEXITED(status))
 return (WEXITSTATUS(status));
 return (1);
 }
-}
-}
 
-fprintf(stderr, "%s: 1: %s: not found\n", prog_name, args[0]);
-return (127);
+return (0);
 }
