75ef35f337b1e2c3f74b12101799545392cf5b25
nizarmaharrak8 <nizarmaharrak@gmail.com>
2025-12-31 03:25:47 +0000

    handling for command execution

--- path.c
+++ path.c
@@ -9,38 +9,47 @@ char *find_executable(char *command)
 {
 struct stat st;
 char *path, *path_copy, *dir, *full_path;
+
 if (!command || command[0] == '\0')
 return (NULL);
-if (is_path_command(command))
+
+if (strchr(command, '/') != NULL)
 {
 if (stat(command, &st) == 0 && (st.st_mode & S_IXUSR))
 return (_strdup(command));
 return (NULL);
 }
-path = _getenv("PATH");
-/* If PATH is NULL (not set), command not found */
-if (path == NULL)
+
+path = getenv("PATH");
+if (path == NULL || path[0] == '\0')
 return (NULL);
+
 path_copy = _strdup(path);
 if (!path_copy)
 return (NULL);
+
 dir = strtok(path_copy, ":");
 while (dir != NULL)
 {
-full_path = build_full_path(dir, command);
+if (dir[0] != '\0')
+{
+full_path = malloc(strlen(dir) + strlen(command) + 2);
 if (!full_path)
 {
 free(path_copy);
 return (NULL);
 }
+sprintf(full_path, "%s/%s", dir, command);
 if (stat(full_path, &st) == 0 && (st.st_mode & S_IXUSR))
 {
 free(path_copy);
 return (full_path);
 }
 free(full_path);
+}
 dir = strtok(NULL, ":");
 }
+
 free(path_copy);
 return (NULL);
 }
