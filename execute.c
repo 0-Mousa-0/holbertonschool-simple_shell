e9c6c95d99ce98db4994f994e875c7b2590c8e79
0-Mousa-0 <mosa.alqarni01@gmail.com>
2025-12-31 12:43:00 +0300

    Ensure fork() is not called for non-existent commands

--- execute.c
+++ execute.c
@@ -1,79 +1,44 @@
 #include "shell.h"
 
-int execute_command(char **args, char *prog_name)
+/**
+ * execute_command - Execute a command with PATH resolution
+ * @args: Array of command and arguments
+ * Return: Status code
+ */
+int execute_command(char **args)
 {
 pid_t pid;
 int status;
 char *full_path;
 
-if (args[0] == NULL)
-return (0);
-
-if (_strcmp(args[0], "exit") == 0)
-return (0);
-
-/* Check if command contains '/' */
-if (has_slash(args[0]))
-{
-/* Absolute or relative path - check if exists */
-if (access(args[0], F_OK) == 0)
-{
-/* Command exists - fork and execute */
-pid = fork();
-if (pid == 0)
-{
-if (execve(args[0], args, environ) == -1)
-{
-fprintf(stderr, "%s: 1: %s: not found\n", prog_name, args[0]);
-_exit(127);
-}
-}
-else if (pid < 0)
-{
-perror(prog_name);
+if (!args || !args[0])
 return (1);
-}
-else
-{
-waitpid(pid, &status, 0);
-if (WIFEXITED(status))
-return (WEXITSTATUS(status));
-return (1);
-}
-}
-else
-{
-/* Command with '/' doesn't exist - NO FORK */
-fprintf(stderr, "%s: 1: %s: not found\n", prog_name, args[0]);
-return (127);
-}
-}
 
-/* Command without '/' - search in PATH */
-full_path = find_in_path(args[0]);
-if (full_path == NULL)
+if (is_builtin(args[0]))
+return (handle_builtin(args));
+
+full_path = find_command(args[0]);
+if (!full_path)
 {
-/* Command not found in PATH - NO FORK */
-fprintf(stderr, "%s: 1: %s: not found\n", prog_name, args[0]);
+print_error(args[0]);
 return (127);
 }
 
-/* Command found - fork and execute */
 pid = fork();
-if (pid == 0)
+if (pid < 0)
+{
+perror("fork");
+free(full_path);
+return (1);
+}
+else if (pid == 0)
 {
 if (execve(full_path, args, environ) == -1)
 {
+print_error(args[0]);
 free(full_path);
-fprintf(stderr, "%s: 1: %s: not found\n", prog_name, args[0]);
-_exit(127);
+exit(127);
 }
-}
-else if (pid < 0)
-{
-free(full_path);
-perror(prog_name);
-return (1);
 }
 else
 {
@@ -81,8 +46,7 @@ waitpid(pid, &status, 0);
 free(full_path);
 if (WIFEXITED(status))
 return (WEXITSTATUS(status));
-return (1);
 }
 
-return (0);
+return (1);
 }
