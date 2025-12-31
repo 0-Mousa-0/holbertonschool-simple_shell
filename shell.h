13c83b1d1e3711dac869111adf7cdae1f93d8f79
0-Mousa-0 <mosa.alqarni01@gmail.com>
2025-12-31 10:44:09 +0300

    PATH handling prototypes

--- shell.h
+++ shell.h
@@ -8,6 +8,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <errno.h>
 
 extern char **environ;
 
@@ -16,7 +17,12 @@ char *read_input(void);
 char **parse_input(char *input);
 int execute_command(char **args, char *prog_name);
 void free_args(char **args);
-int is_builtin(char *command);
 int _strcmp(char *s1, char *s2);
+int _strlen(char *s);
+char *_strdup(char *str);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+int has_slash(char *command);
+char *find_in_path(char *command);
 
 #endif
