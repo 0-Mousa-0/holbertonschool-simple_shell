a06b916a499af6f6f68f54d5e51651a452b4dd9b
0-Mousa-0 <mosa.alqarni01@gmail.com>
2025-12-31 13:29:18 +0300

    return NULL for empty PATH and prevent search when PATH is unset

--- path.c
+++ path.c
@@ -2,15 +2,18 @@
 
 /**
  * find_path - Finds the executable path for a command
- * @command: Command to find
- * Return: Full path or NULL if not found
+ * @command: Command to find (e.g., "ls")
+ * Return: Full path (e.g., "/bin/ls") or NULL if not found
  */
 char *find_path(char *command)
 {
 	char *path, *path_cp, *token, *file_path;
 	struct stat st;
 
-	/* Case: Command is a direct path (e.g., /bin/ls or ./ls or ../ls) */
+	if (!command)
+		return (NULL);
+
+	/* Case: Command is a direct path (e.g., /bin/ls or ./hbtn_ls) */
 	if (strchr(command, '/'))
 	{
 		if (stat(command, &st) == 0)
@@ -18,7 +21,10 @@ char *find_path(char *command)
 		return (NULL);
 	}
 
+	/* Get PATH variable */
 	path = _getenv("PATH");
+
+	/* CASE: PATH is removed, set to empty, or command is empty */
 	if (!path || _strlen(path) == 0)
 		return (NULL);
 
@@ -26,7 +32,10 @@ char *find_path(char *command)
 	token = strtok(path_cp, ":");
 	while (token != NULL)
 	{
+		/* Construct: folder/command */
 		file_path = malloc(_strlen(token) + _strlen(command) + 2);
+		if (!file_path)
+			break;
 		_strcpy(file_path, token);
 		_strcat(file_path, "/");
 		_strcat(file_path, command);
