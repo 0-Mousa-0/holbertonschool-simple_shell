49eace87d46e47948ad4ccebdf976509275b5a72
0-Mousa-0 <mosa.alqarni01@gmail.com>
2025-12-31 10:48:22 +0300

    Parser recognizes exit as built-in command

--- parser.c
+++ parser.c
@@ -1,10 +1,5 @@
 #include "shell.h"
 
-/**
- * parse_input - Parse input line into arguments
- * @input: Input string
- * Return: Array of arguments or NULL
- */
 char **parse_input(char *input)
 {
 int bufsize = 64;
@@ -20,7 +15,7 @@ while (token != NULL)
 {
 if (token[0] != '\0')
 {
-args[position] = strdup(token);
+args[position] = _strdup(token);
 if (!args[position])
 {
 free_args(args);
@@ -41,36 +36,4 @@ token = strtok(NULL, " \t\r\n");
 
 args[position] = NULL;
 return (args);
-}
-
-/**
- * is_builtin - Check if command is built-in
- * @command: Command to check
- * Return: 1 if built-in, 0 otherwise
- */
-int is_builtin(char *command)
-{
-if (!command)
-return (0);
-
-if (_strcmp(command, "exit") == 0)
-return (1);
-
-return (0);
-}
-
-/**
- * _strcmp - Compare strings
- * @s1: First string
- * @s2: Second string
- * Return: 0 if equal, difference otherwise
- */
-int _strcmp(char *s1, char *s2)
-{
-int i = 0;
-
-while (s1[i] && s2[i] && s1[i] == s2[i])
-i++;
-
-return (s1[i] - s2[i]);
 }
